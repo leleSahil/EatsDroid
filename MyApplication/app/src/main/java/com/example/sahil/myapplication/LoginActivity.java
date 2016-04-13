10c62f85b01a9f7898127dd03ab31af25afb02a6
cherriewang <cherrie.cherry@gmail.com>
2016-04-12 23:46:47 -0700

    Implementing login functionality through OAuth2.0

---
+++ MyApplication/app/src/main/java/com/example/sahil/myapplication/LoginActivity.java
@@ -0,0 +1,504 @@
+package com.example.sahil.myapplication;
+
+//import android.animation.Animator;
+//import android.animation.AnimatorListenerAdapter;
+//import android.annotation.TargetApi;
+//import android.content.pm.PackageManager;
+//import android.support.annotation.NonNull;
+//import android.support.design.widget.Snackbar;
+//import android.support.v7.app.AppCompatActivity;
+//import android.app.LoaderManager.LoaderCallbacks;
+//
+//import android.content.CursorLoader;
+//import android.content.Loader;
+//import android.database.Cursor;
+//import android.net.Uri;
+//import android.os.AsyncTask;
+//
+//import android.os.Build;
+//import android.os.Bundle;
+//import android.provider.ContactsContract;
+//import android.text.TextUtils;
+//import android.view.KeyEvent;
+//import android.view.View;
+//import android.view.View.OnClickListener;
+//import android.view.inputmethod.EditorInfo;
+//import android.widget.ArrayAdapter;
+//import android.widget.AutoCompleteTextView;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.TextView;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import static android.Manifest.permission.READ_CONTACTS;
+//
+///**
+// * A login screen that offers login via email/password.
+// */
+//public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+//
+//    /**
+//     * Id to identity READ_CONTACTS permission request.
+//     */
+//    private static final int REQUEST_READ_CONTACTS = 0;
+//
+//    /**
+//     * A dummy authentication store containing known user names and passwords.
+//     * TODO: remove after connecting to a real authentication system.
+//     */
+//    private static final String[] DUMMY_CREDENTIALS = new String[]{
+//            "foo@example.com:hello", "bar@example.com:world"
+//    };
+//    /**
+//     * Keep track of the login task to ensure we can cancel it if requested.
+//     */
+//    private UserLoginTask mAuthTask = null;
+//
+//    // UI references.
+//    private AutoCompleteTextView mEmailView;
+//    private EditText mPasswordView;
+//    private View mProgressView;
+//    private View mLoginFormView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+//        // Set up the login form.
+//        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+//        populateAutoComplete();
+//
+//        mPasswordView = (EditText) findViewById(R.id.password);
+//        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+//                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+//                    attemptLogin();
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
+//
+//        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+//        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                attemptLogin();
+//            }
+//        });
+//
+//        mLoginFormView = findViewById(R.id.login_form);
+//        mProgressView = findViewById(R.id.login_progress);
+//    }
+//
+//    private void populateAutoComplete() {
+//        if (!mayRequestContacts()) {
+//            return;
+//        }
+//
+//        getLoaderManager().initLoader(0, null, this);
+//    }
+//
+//    private boolean mayRequestContacts() {
+//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+//            return true;
+//        }
+//        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+//            return true;
+//        }
+//        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
+//            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
+//                    .setAction(android.R.string.ok, new View.OnClickListener() {
+//                        @Override
+//                        @TargetApi(Build.VERSION_CODES.M)
+//                        public void onClick(View v) {
+//                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+//                        }
+//                    });
+//        } else {
+//            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+//        }
+//        return false;
+//    }
+//
+//    /**
+//     * Callback received when a permissions request has been completed.
+//     */
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+//                                           @NonNull int[] grantResults) {
+//        if (requestCode == REQUEST_READ_CONTACTS) {
+//            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                populateAutoComplete();
+//            }
+//        }
+//    }
+//
+//
+//    /**
+//     * Attempts to sign in or register the account specified by the login form.
+//     * If there are form errors (invalid email, missing fields, etc.), the
+//     * errors are presented and no actual login attempt is made.
+//     */
+//    private void attemptLogin() {
+//        if (mAuthTask != null) {
+//            return;
+//        }
+//
+//        // Reset errors.
+//        mEmailView.setError(null);
+//        mPasswordView.setError(null);
+//
+//        // Store values at the time of the login attempt.
+//        String email = mEmailView.getText().toString();
+//        String password = mPasswordView.getText().toString();
+//
+//        boolean cancel = false;
+//        View focusView = null;
+//
+//        // Check for a valid password, if the user entered one.
+//        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+//            mPasswordView.setError(getString(R.string.error_invalid_password));
+//            focusView = mPasswordView;
+//            cancel = true;
+//        }
+//
+//        // Check for a valid email address.
+//        if (TextUtils.isEmpty(email)) {
+//            mEmailView.setError(getString(R.string.error_field_required));
+//            focusView = mEmailView;
+//            cancel = true;
+//        } else if (!isEmailValid(email)) {
+//            mEmailView.setError(getString(R.string.error_invalid_email));
+//            focusView = mEmailView;
+//            cancel = true;
+//        }
+//
+//        if (cancel) {
+//            // There was an error; don't attempt login and focus the first
+//            // form field with an error.
+//            focusView.requestFocus();
+//        } else {
+//            // Show a progress spinner, and kick off a background task to
+//            // perform the user login attempt.
+//            showProgress(true);
+//            mAuthTask = new UserLoginTask(email, password);
+//            mAuthTask.execute((Void) null);
+//        }
+//    }
+//
+//    private boolean isEmailValid(String email) {
+//        //TODO: Replace this with your own logic
+//        return email.contains("@");
+//    }
+//
+//    private boolean isPasswordValid(String password) {
+//        //TODO: Replace this with your own logic
+//        return password.length() > 4;
+//    }
+//
+//    /**
+//     * Shows the progress UI and hides the login form.
+//     */
+//    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+//    private void showProgress(final boolean show) {
+//        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+//        // for very easy animations. If available, use these APIs to fade-in
+//        // the progress spinner.
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+//            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+//
+//            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+//            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+//                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+//                @Override
+//                public void onAnimationEnd(Animator animation) {
+//                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+//                }
+//            });
+//
+//            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+//            mProgressView.animate().setDuration(shortAnimTime).alpha(
+//                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+//                @Override
+//                public void onAnimationEnd(Animator animation) {
+//                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+//                }
+//            });
+//        } else {
+//            // The ViewPropertyAnimator APIs are not available, so simply show
+//            // and hide the relevant UI components.
+//            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+//            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+//        }
+//    }
+//
+//    @Override
+//    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+//        return new CursorLoader(this,
+//                // Retrieve data rows for the device user's 'profile' contact.
+//                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
+//                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
+//
+//                // Select only email addresses.
+//                ContactsContract.Contacts.Data.MIMETYPE +
+//                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
+//                .CONTENT_ITEM_TYPE},
+//
+//                // Show primary email addresses first. Note that there won't be
+//                // a primary email address if the user hasn't specified one.
+//                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
+//    }
+//
+//    @Override
+//    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+//        List<String> emails = new ArrayList<>();
+//        cursor.moveToFirst();
+//        while (!cursor.isAfterLast()) {
+//            emails.add(cursor.getString(ProfileQuery.ADDRESS));
+//            cursor.moveToNext();
+//        }
+//
+//        addEmailsToAutoComplete(emails);
+//    }
+//
+//    @Override
+//    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+//
+//    }
+//
+//    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
+//        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
+//        ArrayAdapter<String> adapter =
+//                new ArrayAdapter<>(LoginActivity.this,
+//                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
+//
+//        mEmailView.setAdapter(adapter);
+//    }
+//
+//
+//    private interface ProfileQuery {
+//        String[] PROJECTION = {
+//                ContactsContract.CommonDataKinds.Email.ADDRESS,
+//                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
+//        };
+//
+//        int ADDRESS = 0;
+//        int IS_PRIMARY = 1;
+//    }
+//
+//    /**
+//     * Represents an asynchronous login/registration task used to authenticate
+//     * the user.
+//     */
+//    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+//
+//        private final String mEmail;
+//        private final String mPassword;
+//
+//        UserLoginTask(String email, String password) {
+//            mEmail = email;
+//            mPassword = password;
+//        }
+//
+//        @Override
+//        protected Boolean doInBackground(Void... params) {
+//            // TODO: attempt authentication against a network service.
+//
+//            try {
+//                // Simulate network access.
+//                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//                return false;
+//            }
+//
+//            for (String credential : DUMMY_CREDENTIALS) {
+//                String[] pieces = credential.split(":");
+//                if (pieces[0].equals(mEmail)) {
+//                    // Account exists, return true if the password matches.
+//                    return pieces[1].equals(mPassword);
+//                }
+//            }
+//
+//            // TODO: register the new account here.
+//            return true;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(final Boolean success) {
+//            mAuthTask = null;
+//            showProgress(false);
+//
+//            if (success) {
+//                finish();
+//            } else {
+//                mPasswordView.setError(getString(R.string.error_incorrect_password));
+//                mPasswordView.requestFocus();
+//            }
+//        }
+//
+//        @Override
+//        protected void onCancelled() {
+//            mAuthTask = null;
+//            showProgress(false);
+//        }
+//    }
+//}
+
+
+/* attempt at webview here */
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity {
+
+    private static String CLIENT_ID = "820762424096-qr8fr039sqs5nvch7qhfa3juu81k4r96.apps.googleusercontent.com";
+    //Use your own client id
+    private static String CLIENT_SECRET ="LH02vu5MmowRRBpzRrLLH1tA";
+    //Use your own client secret
+    private static String REDIRECT_URI="http://localhost";
+    private static String GRANT_TYPE="authorization_code";
+    private static String TOKEN_URL ="https://accounts.google.com/o/oauth2/token";
+    private static String OAUTH_URL ="https://accounts.google.com/o/oauth2/auth";
+    private static String OAUTH_SCOPE="https://www.googleapis.com/auth/urlshortener";
+    //Change the Scope as you need
+    WebView web;
+    Button auth;
+    SharedPreferences pref;
+    TextView Access;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        pref = getSharedPreferences("AppPref", MODE_PRIVATE);
+        Access =(TextView)findViewById(R.id.Access);
+        auth = (Button)findViewById(R.id.auth);
+        auth.setOnClickListener(new View.OnClickListener() {
+            Dialog auth_dialog;
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                auth_dialog = new Dialog(LoginActivity.this);
+                auth_dialog.setContentView(R.layout.auth_dialog);
+                web = (WebView)auth_dialog.findViewById(R.id.webv);
+                web.getSettings().setJavaScriptEnabled(true);
+                web.loadUrl(OAUTH_URL+"?redirect_uri="+REDIRECT_URI+"&response_type=code&client_id="+CLIENT_ID+"&scope="+OAUTH_SCOPE);
+                web.setWebViewClient(new WebViewClient() {
+
+                    boolean authComplete = false;
+                    Intent resultIntent = new Intent();
+
+                    @Override
+                    public void onPageStarted(WebView view, String url, Bitmap favicon){
+                        super.onPageStarted(view, url, favicon);
+
+                    }
+                    String authCode;
+                    @Override
+                    public void onPageFinished(WebView view, String url) {
+                        super.onPageFinished(view, url);
+
+                        if (url.contains("?code=") && authComplete != true) {
+                            Uri uri = Uri.parse(url);
+                            authCode = uri.getQueryParameter("code");
+                            Log.i("", "CODE : " + authCode);
+                            authComplete = true;
+                            resultIntent.putExtra("code", authCode);
+                            LoginActivity.this.setResult(Activity.RESULT_OK, resultIntent);
+                            setResult(Activity.RESULT_CANCELED, resultIntent);
+
+                            SharedPreferences.Editor edit = pref.edit();
+                            edit.putString("Code", authCode);
+                            edit.commit();
+                            auth_dialog.dismiss();
+                            new TokenGet().execute();
+                            Toast.makeText(getApplicationContext(),"Authorization Code is: " +authCode, Toast.LENGTH_SHORT).show();
+                        }else if(url.contains("error=access_denied")){
+                            Log.i("", "ACCESS_DENIED_HERE");
+                            resultIntent.putExtra("code", authCode);
+                            authComplete = true;
+                            setResult(Activity.RESULT_CANCELED, resultIntent);
+                            Toast.makeText(getApplicationContext(), "Error Occured", Toast.LENGTH_SHORT).show();
+
+                            auth_dialog.dismiss();
+                        }
+                    }
+                });
+                auth_dialog.show();
+                auth_dialog.setTitle("Authorize Learn2Crack");
+                auth_dialog.setCancelable(true);
+            }
+        });
+    }
+
+    private class TokenGet extends AsyncTask<String, String, JSONObject> {
+        private ProgressDialog pDialog;
+        String Code;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(LoginActivity.this);
+            pDialog.setMessage("Contacting Google ...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            Code = pref.getString("Code", "");
+            pDialog.show();
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... args) {
+            GetAccessToken jParser = new GetAccessToken();
+            JSONObject json = jParser.gettoken(TOKEN_URL,Code,CLIENT_ID,CLIENT_SECRET,REDIRECT_URI,GRANT_TYPE);
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            pDialog.dismiss();
+            if (json != null){
+
+                try {
+
+                    String tok = json.getString("access_token");
+                    String expire = json.getString("expires_in");
+                    String refresh = json.getString("refresh_token");
+
+                    Log.d("Token Access", tok);
+                    Log.d("Expire", expire);
+                    Log.d("Refresh", refresh);
+                    auth.setText("Authenticated");
+                    Access.setText("Access Token:"+tok+"nExpires:"+expire+"nRefresh Token:"+refresh);
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }else{
+                Toast.makeText(getApplicationContext(), "Network Error", Toast.LENGTH_SHORT).show();
+                pDialog.dismiss();
+            }
+        }
+    }
+
+}
