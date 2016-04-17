6af75bcb3ff728237dc0615344b4fa828e7556d3
Riley Testut <riley@rileytestut.com>
2016-04-16 20:57:26 -0700

    Removes unused imports

--- MyApplication/app/src/main/java/com/example/sahil/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/sahil/myapplication/MainActivity.java
@@ -17,17 +17,13 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
 import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
-import com.example.sahil.myapplication.FeastObjects.FeastObjects.Restaurant;
 import com.example.sahil.myapplication.Utils.CalendarUtils;
-import com.example.sahil.myapplication.Utils.Constants;
 
 import java.util.Calendar;
-import java.util.Date;
 
 import butterknife.Bind;
 
