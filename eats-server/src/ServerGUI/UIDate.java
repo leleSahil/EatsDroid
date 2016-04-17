41ae593c0bece41daf4a8c9328c0b07e41866efc
Vincent Bertoni <vbertoni@usc.edu>
2016-04-17 14:40:08 -0700

    basic skeleton of the UI, still need to implement Client object onto it

---
+++ eats-server/src/ServerGUI/UIDate.java
@@ -0,0 +1,17 @@
+package ServerGUI;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UIDate{
+	private Date d;
+	private static SimpleDateFormat sdf = new SimpleDateFormat("EEEEE, MMMMMM dd");
+	
+	public UIDate(Date d) {
+		this.d = d;
+	}
+	
+	public String toString() {
+		return sdf.format(d);
+	}
+}
